61094dffe822f6f1f3d7af883229475dfff7e98a
Diego Acero <diegoaceroarguelles@gmail.com>
2024-10-06 20:43:59 +0200

    add github actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
